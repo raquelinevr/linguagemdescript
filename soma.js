519341755d2980930f369d6947c681b5a7c6e9cd
Raqueline Gomes <55905389+raquelinevr@users.noreply.github.com>
2021-02-14 11:56:28 -0300

    Update soma.js

--- soma.js
+++ soma.js
@@ -1,2 +1,22 @@
-const name = ("Raqueline")
-console.log("Oi")
+// Soma.
+// Crie uma função para calcular a soma de dois valores, por exemplo, se recebermos os valores 1 e 2 então o resultado será 3. Para analisar mais exemplos veja a Tabela 1.
+// Entrada: 1, 2 => 3
+// Entrada: 3, 2 => 5
+
+//1
+
+function soma(x, y) {
+  return x + y;
+}
+
+let valor1 = parseInt(prompt("Primeiro valor:  "));
+let valor2 = parseInt(prompt("Segundo valor: "));
+
+let resultado = soma(valor1, valor2);
+
+console.log("Resultado:  " + resultado);
+
+
+//2 
+
+
