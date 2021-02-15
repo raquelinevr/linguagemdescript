a20d4943905542e0c055ddce65e822d9cb3f6c1c
Raqueline Gomes <55905389+raquelinevr@users.noreply.github.com>
2021-02-15 18:55:39 -0300

    Create ex03.js

---
+++ Exercícios/ex03.js
@@ -0,0 +1,10 @@
+// Área do Círculo
+
+function areaCirculo(raio){
+  const pi = 3.1415
+  area = pi * (raio ** 2)
+  return area
+}
+
+console.log(areaCirculo(10)); // => 314.15....
+console.log(areaCirculo(1)); // => 3.1415....
